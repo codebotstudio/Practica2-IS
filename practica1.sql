0a4a0dc0c831f71c26f25d00931ba137cf655e2f
Daniel Monroy <ldanielmonroy@gmail.com>
2016-03-22 20:22:07 -0600

    Inicia aquí Practica 2

---
+++ practica1.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE usuario (
+nombre varchar(25) NOT NULL,
+apellido varchar(25) NOT NULL,
+username varchar(25) PRIMARY KEY,
+password varchar(25) NOT NULL,
+email varchar(25) NOT NULL
+);
+
+CREATE TABLE estudiante (
+username varchar(25) references usuario(username),
+materia varchar(30) NOT NULL,
+escolaridad varchar(30) NOT NULL
+);
